336a192170ba47e3b63af92b52308ffc6dd49288
Mikail Gezer <Mikail Gezer@DESKTOP-71FJKJN>
2023-10-14 13:49:59 +0300

    Add project files.

---
+++ direnc_hesaplama/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace direnc_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblTolDeger = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(949, 253);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kirmizi",
+            "Turuncu",
+            "Sari",
+            "Yesil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 320);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kirmizi",
+            "Turuncu",
+            "Sari",
+            "Yesil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox2.Location = new System.Drawing.Point(225, 320);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 28);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Siyah",
+            "Kahverengi",
+            "Kirmizi",
+            "Turuncu",
+            "Sari",
+            "Yesil",
+            "Mavi",
+            "Mor",
+            "Gri",
+            "Beyaz"});
+            this.comboBox3.Location = new System.Drawing.Point(424, 320);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(176, 28);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Altin",
+            "Gumus"});
+            this.comboBox4.Location = new System.Drawing.Point(626, 320);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(180, 28);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(178, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 71);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(441, 468);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Direnç Değeri:";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(571, 468);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(29, 20);
+            this.lblSonuc.TabIndex = 7;
+            this.lblSonuc.Text = "00";
+            // 
+            // lblTolDeger
+            // 
+            this.lblTolDeger.AutoSize = true;
+            this.lblTolDeger.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTolDeger.Location = new System.Drawing.Point(571, 502);
+            this.lblTolDeger.Name = "lblTolDeger";
+            this.lblTolDeger.Size = new System.Drawing.Size(29, 20);
+            this.lblTolDeger.TabIndex = 9;
+            this.lblTolDeger.Text = "00";
+            this.lblTolDeger.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(424, 502);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tolerans Değeri:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 32);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 32);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(424, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 32);
+            this.button4.TabIndex = 12;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(626, 363);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 32);
+            this.button5.TabIndex = 13;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(966, 593);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblTolDeger);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblTolDeger;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ direnc_hesaplama/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace direnc_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string renk1;
+            string renk2;
+            string carpanrenk;
+            string toleransrenk;
+
+            double  renk1deg = 0;
+            double  renk2deg = 0;
+            double  carpandeg =0;
+            double  tolerans =0 ;
+
+            renk1 = comboBox1.SelectedItem.ToString();
+            renk2 = comboBox2.SelectedItem.ToString();
+            carpanrenk  = comboBox3.SelectedItem.ToString();
+            toleransrenk  = comboBox4.SelectedItem.ToString();
+
+            double direncdeg = 0;
+            double toldeg = 0;
+
+
+            switch (renk1)
+            {
+                case "Siyah":
+                    renk1deg = 0;
+                    break;
+                case "Kahverengi":
+                    renk1deg = 1;
+                    break;
+                case "Kirmizi":
+                    renk1deg = 2;
+                    break;
+                case "Turuncu":
+                    renk1deg = 3;
+                    break;
+                case "Sari":
+                    renk1deg = 4;
+                    break;
+                case "Yesil":
+                    renk1deg = 5;
+                    break;
+                case "Mavi":
+                    renk1deg = 6;
+                    break;
+                case "Mor":
+                    renk1deg = 7;
+                    break;
+                case "Gri":
+                    renk1deg = 8;
+                    break;
+                case "Beyaz":
+                    renk1deg = 9;
+                    break;
+                default:
+                    MessageBox.Show("İlk rengi giriniz.");
+                    break;
+            }
+
+            switch (renk2)
+            {
+                case "Siyah":
+                    renk2deg = 0;
+                    break;
+                case "Kahverengi":
+                    renk2deg = 1;
+                    break;
+                case "Kirmizi":
+                    renk2deg = 2;
+                    break;
+                case "Turuncu":
+                    renk2deg = 3;
+                    break;
+                case "Sari":
+                    renk2deg = 4;
+                    break;
+                case "Yesil":
+                    renk2deg = 5;
+                    break;
+                case "Mavi":
+                    renk2deg = 6;
+                    break;
+                case "Mor":
+                    renk2deg = 7;
+                    break;
+                case "Gri":
+                    renk2deg = 8;
+                    break;
+                case "Beyaz":
+                    renk2deg = 9;
+                    break;
+                default:
+                    MessageBox.Show("İkinci rengi giriniz.");
+                    break;
+            }
+
+            switch (carpanrenk)
+            {
+                case "Siyah":
+                    carpandeg = 0;
+                    break;
+                case "Kahverengi":
+                    carpandeg = 1;
+                    break;
+                case "Kirmizi":
+                    carpandeg = 2;
+                    break;
+                case "Turuncu":
+                    carpandeg = 3;
+                    break;
+                case "Sari":
+                    carpandeg = 4;
+                    break;
+                case "Yesil":
+                    carpandeg = 5;
+                    break;
+                case "Mavi":
+                    carpandeg = 6;
+                    break;
+                case "Mor":
+                    carpandeg = 7;
+                    break;
+                case "Gri":
+                    carpandeg = 8;
+                    break;
+                case "Beyaz":
+                    carpandeg = 9;
+                    break;
+                default:
+                    MessageBox.Show("Üçüncü rengi giriniz.");
+                    break;
+            }
+
+            switch (toleransrenk )
+            {
+                case "Altin":
+                    tolerans  = 5;
+                    break;
+                case "Gumus":
+                    tolerans = 10;
+                    break;
+                default:
+                    MessageBox.Show("Dördüncü rengi giriniz.");
+                    break;
+            }
+
+            direncdeg = (renk1deg * 10 + renk2deg) * Math.Pow(10, carpandeg);
+
+            lblSonuc.Text = direncdeg.ToString () + "Ω" + "  % " + tolerans .ToString() + "Ω";
+
+            toldeg = direncdeg * (tolerans  / 100);
+            lblTolDeger.Text = toldeg.ToString() + "Ω";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string renk1;
+            renk1 = comboBox1.SelectedItem.ToString();
+
+            switch (renk1)
+            {
+                case "Siyah":
+                    button2.BackColor = Color.Black;
+                    break;
+                case "Kahverengi":
+                    button2.BackColor = Color.Brown;
+                    break;
+                case "Kirmizi":
+                    button2.BackColor = Color.Red;
+                    break;
+                case "Turuncu":
+                    button2.BackColor = Color.OrangeRed;
+                    break;
+                case "Sari":
+                    button2.BackColor = Color.Yellow;
+                    break;
+                case "Yesil":
+                    button2.BackColor = Color.Green;
+                    break;
+                case "Mavi":
+                    button2.BackColor = Color.Blue;
+                    break;
+                case "Mor":
+                    button2.BackColor = Color.Purple;
+                    break;
+                case "Gri":
+                    button2.BackColor = Color.Gray;
+                    break;
+                case "Beyaz":
+                    button2.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string renk2;
+            renk2 = comboBox2.SelectedItem.ToString();
+
+            switch (renk2)
+            {
+                case "Siyah":
+                    button3.BackColor = Color.Black;
+                    break;
+                case "Kahverengi":
+                    button3.BackColor = Color.Brown;
+                    break;
+                case "Kirmizi":
+                    button3.BackColor = Color.Red;
+                    break;
+                case "Turuncu":
+                    button3.BackColor = Color.OrangeRed;
+                    break;
+                case "Sari":
+                    button3.BackColor = Color.Yellow;
+                    break;
+                case "Yesil":
+                    button3.BackColor = Color.Green;
+                    break;
+                case "Mavi":
+                    button3.BackColor = Color.Blue;
+                    break;
+                case "Mor":
+                    button3.BackColor = Color.Purple;
+                    break;
+                case "Gri":
+                    button3.BackColor = Color.Gray;
+                    break;
+                case "Beyaz":
+                    button3.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string carpanrenk;
+            carpanrenk = comboBox3.SelectedItem.ToString();
+
+            switch (carpanrenk )
+            {
+                case "Siyah":
+                    button4.BackColor = Color.Black;
+                    break;
+                case "Kahverengi":
+                    button4.BackColor = Color.Brown;
+                    break;
+                case "Kirmizi":
+                    button4.BackColor = Color.Red;
+                    break;
+                case "Turuncu":
+                    button4.BackColor = Color.OrangeRed;
+                    break;
+                case "Sari":
+                    button4.BackColor = Color.Yellow;
+                    break;
+                case "Yesil":
+                    button4.BackColor = Color.Green;
+                    break;
+                case "Mavi":
+                    button4.BackColor = Color.Blue;
+                    break;
+                case "Mor":
+                    button4.BackColor = Color.Purple;
+                    break;
+                case "Gri":
+                    button4.BackColor = Color.Gray;
+                    break;
+                case "Beyaz":
+                    button4.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string toleransrenk;
+            toleransrenk = comboBox4.SelectedItem.ToString();
+
+            switch (toleransrenk)
+            {
+                case "Altin":
+                    button5.BackColor = Color.Gold;
+                    break;
+                case "Gumus":
+                    button5.BackColor = Color.Silver;
+                    break;
+            }
+        }
+    }
+}
